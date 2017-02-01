d5c0059187f21aef1a51b8d284f93eba075c7a42
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-01 15:09:57 -0700

    Spec for profile show page

---
+++ spec/features/profile/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Viewing a user's profile page" do
+  context "a user has activities" do
+    let!(:strava_user) { create(:user, :strava) }
+    let!(:total_distance) { 38723 }
+    scenario "user sees lifetime stats", :vcr => true do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(strava_user)
+      visit profile_path
+
+      expect(page).to have_current_path(profile_path)
+      expect(page).to have_content "Ride"
+      expect(page).to have_content "Run"
+      expect(page).to have_content "Swim"
+      expect(page).to have_content "Longest Ride"
+      expect(page).to have_content "Total Distance"
+      expect(page).to have_content "Total Moving Time"
+      expect(page).to have_content total_distance
+    end
+  end
+end
