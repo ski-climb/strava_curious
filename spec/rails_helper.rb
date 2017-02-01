fc3ccfec6b414eb67e7da61df3d615b46b87aca5
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-01 14:36:49 -0700

    User can log in spec passing - with VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rack_session_access/capybara'
 require 'vcr'
+require 'capybara/mechanize'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
