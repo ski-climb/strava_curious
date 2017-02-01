1a198571e46f4858ee6d8a6ef2b3ed0a4b6c4b89
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-31 23:19:32 -0700

    Show activities on athlete dashboard

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Auth::SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
 
   def create
     strava_request = "https://www.strava.com/oauth/token?client_id=#{ENV["strava_client_id"]}&client_secret=#{ENV["strava_client_secret"]}&code=#{params["code"]}"
@@ -7,6 +8,7 @@ class Auth::SessionsController < ApplicationController
 
     if user = User.from_strava(user_data["athlete"], user_data["access_token"])
       session[:user_id] = user.id
+      user.update(token: user_data["access_token"])
     end
 
     redirect_to dashboard_path
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,6 +5,5 @@ class DashboardsController < ApplicationController
     # strava_streams = Faraday.get("https://www.strava.com/api/v3/activities/849984063/streams/latlng?access_token=#{current_user.token}&resolution=low")
     @activities = JSON.parse(strava_activities.body)
     # @streams = JSON.parse(strava_streams.body)
-    render json: @activities
   end
 end
