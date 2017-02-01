91d5a70f0e9eae1fcbe80b127a3d73781feee2a4
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-01 14:36:49 -0700

    User can log in spec passing - with VCR

--- spec/features/activities/index_spec.rb
+++ spec/features/activities/index_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe "Viewing a users' activities" do
   context "a user has activities" do
-    let!(:nick) { create(:user, uid: 9685944, token: ENV['ACCESS_TOKEN']) }
+    let!(:strava_user) { create(:user, :strava) }
     let!(:distance_in_meters) { 12904.9 }
     scenario "user sees a list of their activities", :vcr => true do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nick)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(strava_user)
       visit activities_path
 
       expect(page).to have_current_path(activities_path)
