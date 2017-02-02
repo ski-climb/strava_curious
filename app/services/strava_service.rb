56374ef6927a702ffa0496311f7584191c2935a8
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-01 22:49:14 -0700

    Extract Faraday connection in Strava Service

--- app/services/strava_service.rb
+++ app/services/strava_service.rb
@@ -1,12 +1,23 @@
 class StravaService
 
   def self.activities(token)
-    activities = Faraday.get("https://www.strava.com/api/v3/athlete/activities?access_token=#{token}")
+    activities = conn.get("/api/v3/athlete/activities", {access_token: token})
     return JSON.parse(activities.body, symbolize_names: true)
   end
 
   def self.activity(id, token)
-    activity = Faraday.get("https://www.strava.com/api/v3/activities/#{id}?access_token=#{token}")
+    activity = conn.get("/api/v3/activities/#{id}", {access_token: token})
     return JSON.parse(activity.body, symbolize_names: true)
   end
+
+  def self.stats(athlete_id, token)
+    stats = conn.get("/api/v3/athletes/#{athlete_id}/stats", {access_token: token})
+    return JSON.parse(stats.body, symbolize_names: true)
+  end
+
+  private
+
+    def self.conn
+      Faraday.new(url: "https://www.strava.com")
+    end
 end
