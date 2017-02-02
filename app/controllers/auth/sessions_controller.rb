24f4869b5e4af740443e4d1f2978637c26371ffb
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-02 11:33:58 -0700

    Extract Faraday call from Sessions Controller

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -2,14 +2,10 @@ class Auth::SessionsController < ApplicationController
   skip_before_action :require_login, only: [:create]
 
   def create
-    strava_request = "https://www.strava.com/oauth/token?client_id=#{ENV["STRAVA_CLIENT_ID"]}&client_secret=#{ENV["STRAVA_CLIENT_SECRET"]}&code=#{params["code"]}"
-    strava_response = Faraday.post(strava_request)
-    user_data = JSON.parse(strava_response.body)
-
-    if user = User.from_strava(user_data["athlete"], user_data["access_token"])
-      session[:user_id] = user.id
-      user.update(token: user_data["access_token"])
-    end
+    user_data = StravaService.authenticate(params["code"])
+    user = User.from_strava(user_data[:athlete], user_data[:access_token])
+    session[:user_id] = user.id
+    user.update(token: user_data[:access_token])
 
     redirect_to activities_path
   end
--- app/services/strava_service.rb
+++ app/services/strava_service.rb
@@ -2,17 +2,26 @@ class StravaService
 
   def self.activities(token)
     activities = conn.get("/api/v3/athlete/activities", {access_token: token})
-    return JSON.parse(activities.body, symbolize_names: true)
+    JSON.parse(activities.body, symbolize_names: true)
   end
 
   def self.activity(id, token)
     activity = conn.get("/api/v3/activities/#{id}", {access_token: token})
-    return JSON.parse(activity.body, symbolize_names: true)
+    JSON.parse(activity.body, symbolize_names: true)
   end
 
   def self.stats(athlete_id, token)
     stats = conn.get("/api/v3/athletes/#{athlete_id}/stats", {access_token: token})
-    return JSON.parse(stats.body, symbolize_names: true)
+    JSON.parse(stats.body, symbolize_names: true)
+  end
+
+  def self.authenticate(code)
+    user = conn.post("/oauth/token", {
+      client_id: ENV["STRAVA_CLIENT_ID"],
+      client_secret: ENV["STRAVA_CLIENT_SECRET"],
+      code: code
+    })
+    JSON.parse(user.body, symbolize_names: true)
   end
 
   private
